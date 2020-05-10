61f3694bc7f872f5a53e6e4e62e91cae212ca56c
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-10 22:50:34 +0530

    css loader added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Indecision from './components/Indecision';
+import './styles/styles.scss'
 
 ReactDOM.render(
 	<Indecision />,
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
+        }, {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
         }]
     },
     devtool: 'cheap-module-eval-source-map',
