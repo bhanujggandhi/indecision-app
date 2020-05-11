6c91cb0ba03b65eda57089a458d6e970fdc44714
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-11 07:25:27 +0530

    Modal & Mobile styled

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
 		contentLabel="Selected Option"
 		onRequestClose={props.handleOkay}
 		ariaHideApp={false}
+		closeTimeoutMS={200}
+		className="modal"
 	>
-		<h3>Selected Option</h3>
-		{props.selectedOption && <p>{props.selectedOption}</p>}
-		<button onClick={props.handleOkay}>Okay</button>
+		<h3 className="modal__title">Selected Option</h3>
+		{props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+		<button className="button" onClick={props.handleOkay}>Okay</button>
 	</Modal>
 );
 
