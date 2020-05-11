67e2009233898becf0505b4d3ac799fc72f8ab97
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-11 06:13:05 +0530

    buttons styled

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,7 @@ import Option from './Option';
 
 const Options = (props) => (
 	<div>
-		<button onClick={props.handleDeleteOption}>Remove All</button>
+		<button className="button button--link" onClick={props.handleDeleteOption}>Remove All</button>
 		{props.options.length === 0 && <p>Please add an option to get started</p>}
 		{props.options.map((option, index) => {
 			return <Option key={index} optionText={option} handleRemoveOption={props.handleRemoveOption} />;
